04005554abfbcc6cfad7c231e949726e84b540ba
fadhfayi <fadhfayiathallah@gmail.com>
2023-01-17 10:30:21 +0700

    tambah dashboard

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -1,16 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Data Posts - SantriKoding.com</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
-</head>
-<body style="background: lightgray">
 
-    <div class="container mt-5">
+@extends('layout.admin')
+@section('content')
+<h4>Students Data</h4>
+<div class="container mt-5">
         <div class="row">
             <div class="col-md-12">
                 <div class="card border-0 shadow rounded">
@@ -76,6 +68,4 @@
             
         @endif
     </script>
-
-</body>
-</html>
+    @stop
